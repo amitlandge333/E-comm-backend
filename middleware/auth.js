2c6e38f03c7079cea67200c5178c6977c52ae94d
Amit <amitlandge333@gmail.com>
2023-04-07 10:53:22 +0530

    authentication changes

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,7 +5,7 @@ const auth = async (req, res, next) => {
     let token = req.headers["authorization"];
     if (token) {
       token = token.split(" ")[1];
-      const isValid = await Jwt.verify(token, "secreteKey");
+      const isValid = await Jwt.verify(token, process.env.SECRETE_KEY);
       
       if (!isValid) {
         throw new Error({
